9293ccb4ebfa58df9f5fc3525f2376ef6f24dcfc
Aline Bazotti <alinebazotti@yahoo.com.br>
2020-09-20 23:27:31 -0300

    módulos institucional e banner, front 2

--- site/Banner/bannerList.php
+++ site/Banner/bannerList.php
@@ -1,73 +1,39 @@
 <?php
-require_once 'conecta.php';
-
-$countSql = "SELECT COUNT(*) AS total FROM produtos ORDER BY id ASC";
-$dataSql = "SELECT * FROM produtos ORDER BY id ASC";
-
-$qryCount = $PDO->prepare($countSql);
-$qryCount->execute();
-
-$total = $qryCount->fetchColumn();
-
-$qryData = $PDO->prepare($dataSql);
-$qryData->execute();
-
-include 'header.php'
-
+$dados = $metodo->buscarTodosBanners();
 ?>
+<main role="main">
 
-    <header>
-        <div class="row">
-            <div class="col-sm-6">
-                <h2>Produtos</h2>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <?php foreach($dados as $banner): ?>
+            <div class="carousel-item">
+                <img src="<?= $banner['imagem']; ?>" width="1350" height="512">
+                <div class="container">
+                    <div class="carousel-caption text-left">
+                        <h1><?php echo $banner['titulo']; ?></h1>
+                        <p><?php echo $banner['descricao']; ?></p>
+                        <?php if ($banner['botao']){?>
+                            <p>
+                                <a class="btn btn-lg btn-primary" href="<?=$banner['botaoLink']?>" role="button"><?=$banner['botao']?></a>
+                            </p>
+                        <?php } ?>
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-6 text-right h2">
-                <a class="btn btn-primary" href="produtoForm.php"><i class="fa fa-plus"></i> Adicionar</a>
-                <a class="btn btn-default" href="index.php"><i class="fa fa-refresh"></i> Atualizar</a>
-            </div>
+            <?php endforeach; ?>
         </div>
-    </header>
-    <hr>
-    <table class="table table-hover">
-        <thead>
-        <tr>
-            <th width="2%">#</th>
-            <th>Produto</th>
-            <th width="10%">Valor</th>
-            <th width="10%">Imagem</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php if ($total > 0) { ?>
-            <?php while ($produto = $qryData->fetch(PDO::FETCH_ASSOC)) { ?>
-                <tr>
-                    <td><?= $produto['id']; ?></td>
-                    <td><?= $produto['nome']; ?></td>
-                    <td>R$<?= $produto['valor']; ?></td>
-                    <?php if (empty($produto['imagem'])) { ?>
-                        <td><i class="fa fa-times"></td>
-                    <?php } else { ?>
-                        <td><?= '<img class="imgProduto" src="data:image/jpeg;base64,' . base64_encode($produto['imagem']) . '"/>' ?></td>
-                    <?php } ?>
-                    <td class="actions text-right">
-                        <a href="produtoForm.php?id=<?= $produto['id'] ?>&op=2" class="btn btn-sm btn-warning">
-                            <i class="fa fa-pencil"></i> Editar
-                        </a>
-                        <a href="#" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#delete-modal" data-id="<?= $produto['id']; ?>">
-                            <i class="fa fa-trash"></i> Excluir
-                        </a>
-                        <a href="pedidoForm.php?produtoId=<?= $produto['id'] ?>" class="btn btn-sm btn-success"><i class="fa fa-cart-plus"></i> Comprar</a>
-                    </td>
-                </tr>
-            <?php } ?>
-        <?php } else { ?>
-            <tr>
-                <td colspan="6">Nenhum produto cadastrado.</td>
-            </tr>
-        <?php } ?>
-        </tbody>
-    </table>
-
-<?php
-include 'footer.php'
-?>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</main>
