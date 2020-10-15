d31e23886578ef0278047c480556478620855096
Aline Bazotti <alinebazotti@yahoo.com.br>
2020-10-15 17:57:58 -0300

    Ajuste banco de dados

--- Util/agendeseudente.sql
+++ Util/agendeseudente.sql
@@ -28,16 +28,17 @@ CREATE TABLE `usuario` (
   `nome` varchar(35) NOT NULL COMMENT 'Nome completo do usuário',
   `data_nascimento` date DEFAULT NULL,
   `cro` varchar(10) DEFAULT NULL COMMENT 'CRO-UF XX-XXX',
-  `telefone` int(15) DEFAULT NULL,
-  `telefone_s` int(15) DEFAULT NULL COMMENT 'Telefone Secundário',
+  `telefone` varchar(20) DEFAULT NULL,
+  `telefone_s` varchar(20) DEFAULT NULL COMMENT 'Telefone Secundário',
   `status` smallint(1) DEFAULT NULL COMMENT '1 - Ativo\n0 - Inativo.\n\nQuem está inativo não pode fazer login no sistema',
   `email` varchar(35) NOT NULL COMMENT 'e-mail utilizado para fazer o login no admin do site',
   `senha` varchar(100) NOT NULL COMMENT 'Gerada em Sha-1',
   `recuperar_senha_codigo` varchar(255) DEFAULT NULL COMMENT 'Rever esse',
   `assinatura` varchar(255) DEFAULT NULL COMMENT 'Assinatura salva de forma digital, pode ser o link',
   `data_cad` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Data em que o usuário foi cadastrado',
-  `data_alt` timestamp NULL DEFAULT NULL COMMENT 'Data em que houve alteração no cadastro do usuário',
+  `data_alt` timestamp NULL DEFAULT current_timestamp() COMMENT 'Data em que houve alteração no cadastro do usuário',
   `usu_alt` varchar(35) DEFAULT NULL COMMENT 'Nome do usuário que fez a alteração no cadastro desse',
+  `permissoes` varchar(255) DEFAULT NULL COMMENT 'rever',
   `id_grupo` int(10) NOT NULL COMMENT 'ID do grupo de permissões que o usuário pertence',
   PRIMARY KEY (`id`),
   UNIQUE KEY `cpf` (`cpf`),
@@ -45,7 +46,7 @@ CREATE TABLE `usuario` (
   UNIQUE KEY `data_nascimento` (`data_nascimento`),
   KEY `FKUsuario845885` (`id_grupo`),
   CONSTRAINT `FKUsuario845885` FOREIGN KEY (`id_grupo`) REFERENCES `grupo` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Usuários da parte administrativa do site\nDentista e Auxiliar, por exemplo';
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COMMENT='Usuários da parte administrativa do site\nDentista e Auxiliar, por exemplo';
 
 CREATE TABLE `banner` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'Id do banner',
@@ -60,14 +61,14 @@ CREATE TABLE `banner` (
   `usu_alt` varchar(35) DEFAULT NULL COMMENT 'Nome do usuário que alterou o cadastro do banner',
   `data_alt` timestamp NULL DEFAULT NULL COMMENT 'Data que houve alteração no cadastro do banner',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Banners cadastrados no site';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Banners cadastrados no site'
 
 CREATE TABLE `procedimento` (
-  `id` int(10) NOT NULL,
-  `desc` varchar(255) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(255) NOT NULL,
   `duracao_media` time NOT NULL COMMENT 'Quero usar isso para fazer um cálculo automático para a hora de fim na agenda',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Procedimentos realizados';
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COMMENT='Procedimentos realizados';
 
 CREATE TABLE `agenda` (
   `id` int(10) NOT NULL,
@@ -133,7 +134,7 @@ CREATE TABLE `endereco` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Endereços dos pacientes';
 
 CREATE TABLE `institucional` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `desc` varchar(45) NOT NULL COMMENT 'titulo, tipo missão, visão, valor',
   `texto` varchar(255) DEFAULT NULL,
   `imagem` varchar(255) DEFAULT NULL,
@@ -159,7 +160,7 @@ CREATE TABLE `prontuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Prontuário do paciente';
 
 CREATE TABLE `servicos` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `desc` varchar(45) NOT NULL COMMENT 'título do serviço',
   `texto` varchar(255) NOT NULL,
   `status` tinyint(4) NOT NULL COMMENT '1 - Publicado\n0 - Inativo',
@@ -173,20 +174,20 @@ CREATE TABLE `servicos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Serviços oferecidos pela clinica (cabelo, unhas, hidratação e unhas)';
 
 CREATE TABLE `site` (
-  `id` int(1) NOT NULL COMMENT 'Só vai ter um',
+  `id` int(1) NOT NULL AUTO_INCREMENT COMMENT 'Só vai ter um',
   `descricao_site` varchar(20) NOT NULL COMMENT 'é o título: Agende Seu Dente',
   `descricao` varchar(255) DEFAULT NULL COMMENT 'Webapp para gerenciamento de clinica com agenda',
   `logotipo` varchar(255) DEFAULT NULL COMMENT 'o caminho dela',
-  `palavra_chave` varchar(20) DEFAULT NULL COMMENT 'Pode ser os mesmos do tcc',
+  `palavra_chave` varchar(50) DEFAULT NULL COMMENT 'Pode ser os mesmos do tcc',
   `email` varchar(255) DEFAULT NULL COMMENT 'que aparece no site',
   `email_contato` varchar(255) DEFAULT NULL COMMENT 'que recebe os e-mail do site',
   `email_trabalhe` varchar(255) DEFAULT NULL COMMENT 'que recebe os e-mail do trabalhe conosco',
   `url` varchar(21) DEFAULT NULL COMMENT 'agendeseudente.com.br',
-  `logadouro` varchar(255) DEFAULT NULL,
+  `logradouro` varchar(255) DEFAULT NULL,
   `numero` int(10) DEFAULT NULL,
   `complemento` varchar(50) DEFAULT NULL,
   `bairro` varchar(50) DEFAULT NULL,
-  `id_municipio` int(10) NOT NULL,
+  `id_municipio` int(10) DEFAULT NULL,
   `uf_estado` char(2) NOT NULL,
   `cep` int(8) DEFAULT NULL,
   `telefone` int(11) DEFAULT NULL,
@@ -218,19 +219,19 @@ CREATE TABLE `site` (
   KEY `FKSite192440` (`uf_estado`),
   KEY `FKSite388780` (`id_municipio`),
   CONSTRAINT `FKSite192440` FOREIGN KEY (`uf_estado`) REFERENCES `estado` (`uf`),
-  CONSTRAINT `FKSite388780` FOREIGN KEY (`id_municipio`) REFERENCES `municipio` (`id`),
-  CONSTRAINT `FKSite839667` FOREIGN KEY (`id_municipio`) REFERENCES `endereco` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Configurações gerais do site';
+  CONSTRAINT `FKSite388780` FOREIGN KEY (`id_municipio`) REFERENCES `municipio` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COMMENT='Configurações gerais do site'
 
 CREATE TABLE `solicitacao_horario` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `nome` varchar(35) DEFAULT NULL COMMENT 'Nome da pessoa',
   `cpf_cnpj` varchar(30) NOT NULL,
-  `desc` varchar(255) DEFAULT NULL COMMENT 'descrever basicamente o que aconteceu',
-  `idade` tinyint(4) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL COMMENT 'descrever basicamente o que aconteceu',
+  `data_nascimento` date NOT NULL,
   `sente_dor` tinyint(4) DEFAULT NULL COMMENT '1 - Sim\n0 - Não',
   `quanto_tempo` varchar(255) DEFAULT NULL COMMENT 'Está com o sintoma há quanto tempo?',
-  `gravidez` tinyint(4) NOT NULL COMMENT '1 - Sim\n2 - Não',
-  `gravida_sim_semanas` tinyint(4) DEFAULT NULL COMMENT 'Se está gravida, a quantas semanas',
+  `gravidez` tinyint(4) DEFAULT NULL COMMENT '1 - Sim\n2 - Não',
+  `gravida_sim_semanas` varchar(10) DEFAULT NULL COMMENT 'Se está gravida, a quantas semanas',
+  `marcado` tinyint(4) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Isso aqui é pro front, pro paciente no site';
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COMMENT='Isso aqui é pro front, pro paciente no site';
