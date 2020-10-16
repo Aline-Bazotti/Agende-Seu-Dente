72cbb6ca0a48850fa11877390c75fbec71a6ccef
Aline Bazotti <alinebazotti@yahoo.com.br>
2020-10-16 17:48:50 -0300

    rodapé ok

--- site/footer.php
+++ site/footer.php
@@ -1,5 +1,6 @@
 <!-- FOOTER -->
 <footer class="container">
-    <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2017-2020 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    <br><br>
+    <p class="float-right"><a href="#">Voltar ao topo</a></p>
+    <p>&copy; TCC Aline Bazotti - Acad. Curso Ciência da Computação.</p>
 </footer>
