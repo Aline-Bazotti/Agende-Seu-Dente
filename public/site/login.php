e5a5d98111742423ad81d5804a2f5986a4b6e913
Aline Bazotti <alinebazotti@yahoo.com.br>
2020-10-29 18:01:54 -0300

    ajuste login

--- public/site/login.php
+++ public/site/login.php
@@ -11,12 +11,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Login Paciente</title>
+    <title>Login Admin</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/sign-in/">
 
     <!-- Bootstrap core CSS -->
-    <link href="../../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
         .bd-placeholder-img {
@@ -35,18 +35,17 @@
         }
     </style>
     <!-- Custom styles for this template -->
-    <link href="../../css/signin.css" rel="stylesheet">
+    <link href="../css/signin.css" rel="stylesheet">
 </head>
 <body class="text-center">
-    <form class="form-signin" action="controller/login.php" method="post">
-        <img class="mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-        <h1 class="h3 mb-3 font-weight-normal">Por favor faça o login</h1>
-        <label for="inputEmail" class="sr-only">Endereço de E-mail</label>
-        <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Endereço de E-mail" required autofocus>
-        <label for="inputPassword" class="sr-only">Senha</label>
-        <input type="password" id="inputPassword" name="senha" class="form-control" placeholder="Senha" required>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
-    </form>
+<form class="form-signin" action="Pages/controller/login.php" method="post">
+    <img class="mb-4" src="../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+    <h1 class="h3 mb-3 font-weight-normal">Por favor faça o login</h1>
+    <label for="inputEmail" class="sr-only">Endereço de E-mail</label>
+    <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Endereço de E-mail" required autofocus>
+    <label for="inputPassword" class="sr-only">Senha</label>
+    <input type="password" id="inputPassword" name="senha" class="form-control" placeholder="Senha" required>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+</form>
 </body>
 </html>
-
