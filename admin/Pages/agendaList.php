bc76a8445fca3a73ab588df9c6a4d4f821aeb493
Aline Bazotti <alinebazotti@yahoo.com.br>
2021-04-29 09:31:26 -0300

    Agenda da mãe

--- admin/Pages/agendaList.php
+++ admin/Pages/agendaList.php
@@ -12,8 +12,7 @@
     </div>
 </header>
 
-<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23616161&amp;ctz=America%2FSao_Paulo&amp;src=Y19na29zdWZsNDlpamdhZDhwdTFsZzFyMHBoa0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23F6BF26" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-
+<iframe src="https://calendar.google.com/calendar/embed?src=keilabazotti%40gmail.com&ctz=America%2FSao_Paulo" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 <?php
 include "Agenda/solicitacaoUsuarioList.php";
 ?>
